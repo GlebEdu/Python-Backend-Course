f5494e3721db4d0d6c53f2ac91ae7c69bd0f6576
GlebEdu <gvgarbuz@edu.hse.ru>
2024-12-29 00:19:12 +0300

    Changed import

--- projectHW4/test_controllers.py
+++ projectHW4/test_controllers.py
@@ -1,5 +1,5 @@
 import pytest
-from .controllers import operation
+from controllers import operation
 
 @pytest.mark.parametrize("a, b, expected", [(1, 2, 3), (5, -4, 1), (7, 8, 15)])
 def test_operation(a: int, b: int, expected: int) -> None:
