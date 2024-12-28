2334e79d1a276fac1eab3621ecb27ad34e63f04c
GlebEdu <gvgarbuz@edu.hse.ru>
2024-12-28 22:02:02 +0300

    Added /author

--- projectHW4/server.py
+++ projectHW4/server.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def server_info():
     return "My server"
 
+@app.route("/author")
+def author():
+    author = {
+        "name": "Gleb",
+        "course": 2,
+        "age": 19,
+    }
+    return jsonify(author)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
