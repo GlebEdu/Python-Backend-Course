4d822af08f43a073921ddaf051fbc894e125e277
GlebEdu <gvgarbuz@edu.hse.ru>
2025-03-06 23:38:38 +0300

    Изменения в ходе настройки Docker

--- projectHW5-7/backend/config/urls.py
+++ projectHW5-7/backend/config/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
 from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
+def home(request):
+    return HttpResponse("Welcome to the Home Page!")
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -31,6 +35,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
